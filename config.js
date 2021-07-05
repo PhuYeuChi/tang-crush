dc6c501c2c1425691793c0e628959092893f826d
PhuYeuChi <86932104+PhuYeuChi@users.noreply.github.com>
2021-07-05 14:30:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,14 @@ const CONFIG = {
     Mà sao chị cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnIntro: '=))',
+    title: 'Em say chị mất rồi !',
+    desc: 'chị làm ny em nhé ^^ ',
+    btnYes: 'Ok ',
+    btnNo: 'Không:3',
+    question:'Chắc chắn làm ny em nhaa',
+    btnReply: 'Trả lời',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3'
+    messDesc: 'Tối nay chơi game với em nhó.',
+    btnAccept: 'Ok'
 }
